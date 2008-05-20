63089535f4970fdaba47881e9dfb36eafbbb538a
Alan Briolat <alan@xenos.(none)>
2008-05-20 18:00:32 +0100

    Added a 'common functions' library

---
+++ csf/lib/common.php
@@ -0,0 +1,34 @@
+<?php
+/*
+ * CodeScape PHP Framework - A simple PHP web framework
+ * Copyright (C) 2008, Alan Briolat <alan@codescape.net>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/*
+ * Common useful functions
+ */
+
+/*
+ * Generate a type 1 UUID (see RFC4122)
+ * 
+ * This will only work on systems that have e2fsprogs/libuuid installed, and
+ * therefore almost all UNIX-like systems.
+ */
+function uuid1()
+{
+    return trim(exec('uuidgen -t'));
+}
+?>
