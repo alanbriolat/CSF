a7ac88d64405ad3b2af7faed76efea0c3e1316c0
Alan Briolat <alan@codescape.net>
2009-07-07 16:02:19 +0100

    Moved sha256() to helpers/misc library

--- libraries/helpers/misc.php
+++ libraries/helpers/misc.php
@@ -1,33 +1,18 @@
 <?php
 /**
- * CodeScape Framework - Common function library
+ * CodeScape Framework - Miscellaneous functions
  *
  * @package     CSF
  * @author      Alan Briolat <alan@codescape.net>
- * @copyright   (c) 2008, Alan Briolat
+ * @copyright   (c) 2009, Alan Briolat
  * @license     http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
  */
-
-/**
- * Generate a type 1 UUID (see RFC4122)
- * 
- * This will only work on systems that have e2fsprogs/libuuid installed, and
- * therefore almost all UNIX-like systems.  Possibly not the most efficient way 
- * to get a UUID...
- *
- * @return  string
- * @todo    Include PHP UUID library in CSF?
- */
-function uuid1()
-{
-    return trim(exec('uuidgen -t'));
-}
 
 /**
  * Create a SHA256 hash of a string
  * 
  * Uses the PHP mhash library to create SHA256 hashes.  It's better than MD5 
- * and SHA1, and for small amounts of data isn't much slower, so use it!
+ * and SHA1, and for small amounts of data isn't much slower.
  *
  * @param   string  $string     String to hash
  * @return  string  Hexadecimal representation of SHA256 hash
@@ -40,4 +25,3 @@ function sha256($string)
 
     return bin2hex(mhash(MHASH_SHA256, $string));
 }
-?>
